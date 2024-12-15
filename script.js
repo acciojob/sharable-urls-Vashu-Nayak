311f79c053faabd818e5b70e719000d075b51602
Vashu Nayak <97188321+Vashu-Nayak@users.noreply.github.com>
2024-12-15 19:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+let btn=document.getElementById('button')
+btn.addEventListener("click",()=>{
+	let name=document.getElementById('name').value
+	let year=document.getElementById('year').value
+	let url=document.getElementById('url')
+	let baseurl="https://localhost:8080/"
+	let addurl=new URLSearchParams({ name, year }).toString()
+	url.textContent= `${baseurl}?${addurl}`
+})
